648893c63d3985e92e2f9cf1b1857a02c78c62c0
Зозуленко Александр Николаевич (4039262) <zozulenko@dev.vtb.ru>
2022-12-09 16:29:46 +0400

    add services

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-require('dotenv').config();
+
 const mongoose = require('mongoose');
 const router = require('./router/index');
 
@@ -16,11 +17,8 @@ app.use('/api', router);
 
 const start = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
+    await mongoose.connect('mongodb+srv://oduvanio:23061984@cluster0.elkjlh2.mongodb.net/?retryWrites=true&w=majority');
+    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
   } catch (e) {
     console.log(e);
   }
---
+++ server/service/user-service.js
@@ -0,0 +1,15 @@
+const UserModel = require('../models/user-model');
+const bcrypt = require('bcrypt');
+
+class UserService {
+  async registration(email, password) {
+    const candidate = await UserModel.findOne({email});
+    if (candidate) {
+      throw new Error(`Пользователь с почтовым адресом ${email} уже существует`);
+    }
+    const hashPassword = await bcrypt.hash(password, 3);
+    const user = await UserModel.create({email, password: hashPassword});
+  }
+}
+
+module.exports = new UserService();
